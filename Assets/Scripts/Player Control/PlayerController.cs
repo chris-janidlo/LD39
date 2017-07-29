14a0e0283ac97520472ccd87ce3d3dd762becd13
Chris Janidlo <christopherjanidlo@gmail.com>
2017-07-28 23:00:41 -0400

    Level Design, Improved Jumping

--- Assets/Scripts/Player Control/PlayerController.cs
+++ Assets/Scripts/Player Control/PlayerController.cs
@@ -10,10 +10,12 @@ public class PlayerController : MonoBehaviour {
 	public float AirSpeed = 5.0f; //air strafing speed
 	public float HalfHeight = 1.0f; //height from ground to origin
 	public float GroundedMargin = 0.1f; //margin of error for ground check
+	public float JumpDelay = 0.1f; //how long to wait after jumping before being grounded resets jumpCount (needed because you are still grounded a couple frames after jumping)
 
 	private Rigidbody rb;
 	private int jumpCount;
-	private bool willJump = false;
+	private float jumpTime;
+	private float vertSpeed;
 
 	void Start () {
 		rb = GetComponent<Rigidbody>();
@@ -21,31 +23,39 @@ public class PlayerController : MonoBehaviour {
 	}
 	
 	void Update () {
+		if (jumpTime > 0)
+			jumpTime -= Time.deltaTime;
+
 		Vector3 moveDir = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
 		moveDir = transform.TransformDirection(moveDir);
 		moveDir *= isGrounded() ? Speed : AirSpeed;
 
-		rb.velocity = new Vector3(moveDir.x, rb.velocity.y, moveDir.z);
-
-		if (isGrounded())
+		if (isGrounded() && jumpTime <= 0) {
+			Debug.Log("hell yeah");
 			jumpCount = 0;
+		}
+		else
+			Debug.Log("nope");
 
-		Debug.Log(jumpCount);
+		if (Input.GetButtonDown("Jump") && jumpCount < JumpBursts.Length) {
+			if (jumpCount == 0)
+				jumpTime = JumpDelay;
+			Debug.Log(jumpCount);
+			vertSpeed = JumpBursts[jumpCount];
+			jumpCount++;
+		}
+		else
+			vertSpeed = rb.velocity.y;
 
-		if (Input.GetButtonDown("Jump") && jumpCount < JumpBursts.Length)
-			willJump = true;
+		rb.velocity = new Vector3(moveDir.x, vertSpeed, moveDir.z);
 	}
 
 	void FixedUpdate() {
 		rb.AddForce(Vector3.down * Gravity);
-		if (willJump) {
-			willJump = false;
-			rb.AddForce(JumpBursts[jumpCount] * Vector3.up, ForceMode.VelocityChange);
-			jumpCount++;
-		}
 	}
 
 	private bool isGrounded () {
-		return Physics.Raycast(transform.position, Vector3.down, HalfHeight + GroundedMargin);
+		int layerMask = ~(1 << 8);
+		return Physics.Raycast(transform.position, Vector3.down, HalfHeight + GroundedMargin, layerMask);
 	}
 }
