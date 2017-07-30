451da34534b7395bc2c8b614c3c9e045b1965535
Chris Janidlo <christopherjanidlo@gmail.com>
2017-07-30 15:04:18 -0400

    Level design, UI

---
+++ Assets/Scripts/Player Control/TextValue.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextValue : MonoBehaviour {
+
+	private Text value;
+
+	// Use this for initialization
+	void Start () {
+		value = GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		value.text = (SceneManager.manager.TimeScale * 100).ToString("F0") + " %";
+	}
+}
---
+++ Assets/Scripts/Player Control/TimerText.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerText : MonoBehaviour {
+
+	private Text value;
+
+	// Use this for initialization
+	void Start () {
+		value = GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		value.text = ((int)Time.timeSinceLevelLoad / 60).ToString("D2") + ":" + ((int)Time.timeSinceLevelLoad % 60).ToString("D2");
+	}
+}
